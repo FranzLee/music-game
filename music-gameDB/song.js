1b1beee067d5b1555e154b2deac5dbffab99ef79
Franz <franz.e.lee@gmail.com>
2024-03-02 23:52:00 +0800

    Add song.js.

---
+++ music-gameDB/song.js
@@ -0,0 +1,17 @@
+function song() {
+    newShortBlock(5, 390);
+    newShortBlock(4, 400);
+    newShortBlock(3, 408);
+    newShortBlock(2, 414);
+    newShortBlock(4, 424);
+    newShortBlock(5, 433);
+    newShortBlock(3, 442);
+    newShortBlock(5, 493);
+    newShortBlock(6, 502);
+    newShortBlock(4, 512);
+    newShortBlock(5, 522);
+    newShortBlock(3, 531);
+    newShortBlock(4, 539);
+    newShortBlock(2, 547);
+    newShortBlock(5, 552);
+}
